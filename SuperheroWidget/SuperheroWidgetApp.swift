bc774c55d2b9495182e4e22a96f3e411c8d5e7bc
Emrecan <emrecanaslan511@gmail.com>
2024-05-11 21:27:48 +0300

    Initial Commit

---
+++ SuperheroWidget/SuperheroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperheroWidgetApp.swift
+//  SuperheroWidget
+//
+//  Created by APPLE on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperheroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
